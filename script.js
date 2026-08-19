616e8e0692704e4604b78cb5f7dc63994d1e71c8
FSG1 <163757833+FabricioSG1@users.noreply.github.com>
2026-08-19 00:11:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ renderer.outputColorSpace = THREE.SRGBColorSpace;
 const controls = new PointerLockControls(camera, document.body);
 //
 const textureLoader = new THREE.TextureLoader();
-const texturaNube = textureLoader.load("/Imagenes/galeria1.jpg");
+const texturaNube = textureLoader.load("/imagenes/Nube.png");
 const materialNube = new THREE.MeshBasicMaterial({
     map: texturaNube,
     transparent: true,
